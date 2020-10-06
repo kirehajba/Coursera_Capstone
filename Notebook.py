0973698b79494c95e27bacbe5deb89ac3d7cdd24
kirehajba <kire.hajba@gmail.com>
2020-10-06 14:30:49 +0200

    Add files via upload

---
+++ Notebook.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This notebook will be used for the Applied Data Science Capstone
+"""
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
